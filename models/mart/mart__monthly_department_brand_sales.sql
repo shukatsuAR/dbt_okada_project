944220a7e74fdd9569dcbef99bb9f76632185757
Okada Shuhei <shushukatu1307@gmail.com>
2025-12-05 19:17:24 +0900

    mart層 CTEの記述修正

--- models/mart/mart__monthly_department_brand_sales.sql
+++ models/mart/mart__monthly_department_brand_sales.sql
@@ -11,26 +11,38 @@
 }}
 
 with
-    join_orders_and_types as (
+    orders_and_types as (
         select
             monthly.month,
-            monthly.user_type,
-            orders.product_department,
-            orders.product_brand,
-            sum(orders.sales_jpy) as sales,
-            count(distinct monthly.user_id) as payment_uu,
+            user_type,
+            product_department,
+            product_brand,
+            sales_jpy,
+            orders.user_id,
         from {{ ref("int__cleansed_orders") }} as orders
         left join
             {{ ref("int__monthly_registered_user_types") }} as monthly
             on date_trunc(orders.order_time_jst, month) = monthly.month
-        where monthly.user_id is not null
-        group by 1, 2, 3, 4
+    ),
+
+    brand_level_summary as (
+        select
+            month,
+            user_type,
+            product_department,
+            product_brand,
+            sales_jpy,
+            count(distinct user_id) over (
+                partition by month, product_department, product_brand
+            ) as window_payment_uu
+        from orders_and_types
     )
 select
     month,
     user_type,
     product_department as department,
-    if(payment_uu >= 10, product_brand, "その他") as brand,
-    sales,
-    payment_uu
-from join_orders_and_types
+    if(window_payment_uu >= 10, product_brand, "その他") as brand,
+    sum(sales_jpy) as sales,
+    max(window_payment_uu) as payment_uu
+from brand_level_summary
+group by 1, 2, 3, 4
